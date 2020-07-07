ecdca5cd73e6f2345d7618bf826ea91a86d29a94
jamesfdz <jfernandes942@gmail.com>
2020-07-08 01:05:40 +0530

    Initial commit

---
+++ app/src/main/java/com/codechez/blog/MainActivity.java
@@ -0,0 +1,22 @@
+package com.codechez.blog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
